85ae12c7ee202569ca9f7196dce248bef4de2830
VikasMeshram2708 <meshramvikas744@gmail.com>
2024-03-26 23:33:05 +0530

    :bug: created login page.

--- todo/app/(pages)/login/page.tsx
+++ todo/app/(pages)/login/page.tsx
@@ -1,13 +1,58 @@
+"use client";
+import { ChangeEvent, FormEvent, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
+    try {
+      e.preventDefault();
+      const data = {
+        email,
+        password,
+      };
+      const response = await fetch("/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const result = await response.json();
+
+      if (!response.ok) {
+        return toast.error(result?.message);
+      }
+      console.log("result", result);
+      return toast.success(result?.message);
+    } catch (e) {
+      const err = e as Error;
+      return toast.error(err?.message);
+    } finally {
+      setEmail("");
+      setPassword("");
+    }
+  };
+
   return (
     <section className="min-h-screen">
-      <form className="mt-10 max-w-md mx-auto p-5 rounded-md border-2 border-slate-400">
+      <form
+        onSubmit={handleLogin}
+        className="mt-10 max-w-md mx-auto p-5 rounded-md border-2 border-slate-400"
+      >
         <h1 className="text-3xl font-semibold text-center">Login</h1>
         {/* Email */}
         <div className="grid gap-3 mb-5">
           <label htmlFor="email">Email</label>
           <input
             type="text"
+            name="email"
+            value={email}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setEmail(e.target.value)
+            }
             placeholder="Enter name"
             className="input input-bordered"
           />
@@ -18,6 +63,11 @@ export default function Login() {
           <label htmlFor="password">Password</label>
           <input
             type="text"
+            name="password"
+            value={password}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setPassword(e.target.value)
+            }
             placeholder="Enter password"
             className="input input-bordered"
           />
@@ -26,13 +76,14 @@ export default function Login() {
         {/* Button */}
         <div className="mt-5">
           <button
-            type="button"
+            type="submit"
             className="btn btn-md w-full btn-outline btn-accent font-semibold"
           >
             Login
           </button>
         </div>
       </form>
+      <Toaster />
     </section>
   );
 }
